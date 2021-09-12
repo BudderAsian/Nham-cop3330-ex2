ed63cd3467c883f1260fb663714da3c4376ab97c
richa <richardnham2000@gmail.com>
2021-09-12 15:09:00 -0400

    Final Solution

---
+++ src/Main/java/com/example/Main.java
@@ -0,0 +1,22 @@
+package com.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Richard Nham
+
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	Scanner scan = new Scanner(System.in);
+    System.out.printf("What is input string? ");
+    String inputString = scan.nextLine();
+
+    int charNum = inputString.length();
+
+    System.out.printf(inputString + " has %d characters.", charNum);
+    }
+}
